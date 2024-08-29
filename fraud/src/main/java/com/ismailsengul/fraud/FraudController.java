1ac271cf6c20a76625f62fc4079fdcebe65daecd
ismailsengul64 <ismail.sengul@saritay.com>
2024-08-29 21:07:35 +0300

    configured eureka server

--- fraud/src/main/java/com/ismailsengul/fraud/FraudController.java
+++ fraud/src/main/java/com/ismailsengul/fraud/FraudController.java
@@ -1,17 +1,20 @@
 package com.ismailsengul.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudCheckService fraudCheckService;
 
     @GetMapping(path ="{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("Fraud check for customer {} is {}", customerID, isFraudulentCustomer);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
